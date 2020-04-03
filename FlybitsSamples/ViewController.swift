18424eaf316866e674e40fc2859f2c3faf76b47f
Alex Smith <alex.smith@flybits.com>
2020-04-03 16:50:16 -0400

    Create Xcode project named `FlybitsSamples`
    This creates a new project using Xcode 11.4 with Storyboards as the default template.
    This includes targets for unit tests and UITests.

---
+++ FlybitsSamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlybitsSamples
+//
+//  Created by Alex on 2020-04-03.
+//  Copyright © 2020 Flybits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
