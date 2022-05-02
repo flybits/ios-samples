fc468446935a9d3387851bb40390c51914884def
Caio Dias <caio.dias@flybits.com>
2022-05-02 15:38:08 -0400

    Update podfile to use v4 binaries and migrate code to build with v4.x

---
+++ Concierge/Expose-Bank/Expose-Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expose-Bank
+//
+//  Created by Caio on 2022-05-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/FlybitsContextSDK/FlybitsContextSDK.xcframework/ios-arm64/FlybitsContextSDK.framework/Headers/FlybitsContextSDK.h
@@ -0,0 +1,20 @@
+//
+//  FlybitsContextSDK.h
+//  FlybitsContextSDK
+//
+//  Created by Caio on 2019-04-29.
+//  Copyright © 2019 Flybits. All rights reserved.
+//
+
+@import UIKit;
+@import FlybitsSDK;
+
+//! Project version number for FlybitsContextSDK.
+FOUNDATION_EXPORT double FlybitsContextSDKVersionNumber;
+
+//! Project version string for FlybitsContextSDK.
+FOUNDATION_EXPORT const unsigned char FlybitsContextSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlybitsContextSDK/PublicHeader.h>
+
+
