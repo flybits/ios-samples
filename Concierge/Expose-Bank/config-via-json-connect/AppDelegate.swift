0a5fcf31050b44df8e06f983f2c5ff8f3bd4c05a
Caio Dias <caio.dias@flybits.com>
2024-12-17 18:37:59 -0300

    Update the config-via-json-connect app code

--- Concierge/Expose-Bank/config-via-json-connect/AppDelegate.swift
+++ Concierge/Expose-Bank/config-via-json-connect/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FlybitsConcierge
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        Concierge.enableLogging()
+
+        //Call configure on Concierge
+        Concierge.configure(contextPlugins: [], launchOptions: launchOptions)
         return true
     }
 
