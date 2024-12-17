95879b360fb3fcc5a5bc63cf3f9e41b025b49ba0
Caio Dias <caio.dias@flybits.com>
2024-12-17 18:05:53 -0300

    Add config-via-json-connect app

---
+++ Concierge/Expose-Bank/config-via-json-connect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  config-via-json-connect
+//
+//  Created by Caio Dias on 2024-12-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
