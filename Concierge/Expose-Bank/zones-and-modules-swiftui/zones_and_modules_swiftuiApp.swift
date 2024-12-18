ecf754ee017746fed2191a6625833dc4ddd796fc
Caio Dias <caio.dias@flybits.com>
2024-12-18 18:56:01 -0300

    Add zones-and-modules-swiftui app

---
+++ Concierge/Expose-Bank/zones-and-modules-swiftui/zones_and_modules_swiftuiApp.swift
@@ -0,0 +1,21 @@
+//
+//  zones_and_modules_swiftuiApp.swift
+//  zones-and-modules-swiftui
+//
+//  Created by Caio Dias on 2024-12-18.
+//
+
+import SwiftUI
+
+@main
+struct zones_and_modules_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+    init() {
+        print("Testing")
+    }
+}
