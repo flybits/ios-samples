75fea1b180f31712f31db71b0f571d4df55fc7ae
Caio Dias <caio.dias@flybits.com>
2024-12-18 13:56:05 -0300

    Properly set the webservice url

--- Concierge/Expose-Bank/zones-and-modules-storyboard/AppDelegate.swift
+++ Concierge/Expose-Bank/zones-and-modules-storyboard/AppDelegate.swift
@@ -12,10 +12,18 @@ import FlybitsConcierge
 class AppDelegate: UIResponder, UIApplicationDelegate {
     let projectIdentifier: String = "2CE41988-B1D3-4116-98DD-42FFB8754384"
     let gatewayURL: String = "https://api.demo.flybits.com"
+    let webSericeURL: String = "https://static-files-concierge.demo.flybits.com/latest"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        let config = FlybitsConciergeConfiguration.Builder().setProjectId(projectIdentifier).setGatewayUrl(gatewayURL).build()
+        Concierge.enableLogging()
+
+        let config = FlybitsConciergeConfiguration.Builder()
+            .setProjectId(projectIdentifier)
+            .setGatewayUrl(gatewayURL)
+            .setWebService(webSericeURL)
+            .build()
+
         Concierge.configure(configuration: config, contextPlugins: [], launchOptions: launchOptions)
 
         return true
