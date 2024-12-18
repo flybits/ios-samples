954fe603e70c604d241263f0f6a188aeee4f5f61
Caio Dias <caio.dias@flybits.com>
2024-12-18 12:58:48 -0300

    Add the logic for zones-and-modules-storyboard app

--- Concierge/Expose-Bank/zones-and-modules-storyboard/AppDelegate.swift
+++ Concierge/Expose-Bank/zones-and-modules-storyboard/AppDelegate.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import FlybitsConcierge
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    let projectIdentifier: String = "2CE41988-B1D3-4116-98DD-42FFB8754384"
+    let gatewayURL: String = "https://api.demo.flybits.com"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let config = FlybitsConciergeConfiguration.Builder().setProjectId(projectIdentifier).setGatewayUrl(gatewayURL).build()
+        Concierge.configure(configuration: config, contextPlugins: [], launchOptions: launchOptions)
+
         return true
     }
 
--- Concierge/Expose-Bank/zones-and-modules-storyboard/ViewController.swift
+++ Concierge/Expose-Bank/zones-and-modules-storyboard/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import FlybitsConcierge
 
 class ViewController: UIViewController {
+    @IBOutlet private var connectButton: UIButton!
+    @IBOutlet private var disconnectButton: UIButton!
+    @IBOutlet private var showConciergeButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+        updateButtons()
+    }
+
+    @IBAction func connectOnTouchUpInside(sender: UIButton) {
+        Concierge.connect(with: AnonymousConciergeIDP()) { error in
+            if let error = error {
+                print("Failed to connect. Reason \(error.localizedDescription)")
+                self.updateButtons()
+            } else {
+                print("Connected with success")
+                self.updateButtons()
+            }
+        }
+    }
+
+    @IBAction func disconnectOnTouchUpInside(sender: UIButton) {
+        Concierge.disconnect { error in
+            if let error = error {
+                print("Failed to disconnect. Reason \(error.localizedDescription)")
+                self.updateButtons()
+            } else {
+                print("Disconnected with success")
+                self.updateButtons()
+            }
+        }
+    }
+
+    @IBAction func showConciergeOnTouchUpInside(sender: UIButton) {
+        let vc = Concierge.viewController(.configured, params: [], options: [])
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    private func updateButtons() {
+        if Thread.isMainThread {
+            self.connectButton.isHidden = Concierge.isConnected
+            self.disconnectButton.isHidden = !Concierge.isConnected
+            self.showConciergeButton.isEnabled = self.connectButton.isHidden
+        } else {
+            DispatchQueue.main.async {
+                self.connectButton.isHidden = Concierge.isConnected
+                self.disconnectButton.isHidden = !Concierge.isConnected
+                self.showConciergeButton.isEnabled = self.connectButton.isHidden
+            }
+        }
+    }
 }
-
