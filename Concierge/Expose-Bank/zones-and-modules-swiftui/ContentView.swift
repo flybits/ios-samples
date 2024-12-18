735c78117d7bc4d3cb715c1d02f398c7c9e4c96d
Caio Dias <caio.dias@flybits.com>
2024-12-18 18:56:01 -0300

    Add zones-and-modules-swiftui app

---
+++ Concierge/Expose-Bank/zones-and-modules-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zones-and-modules-swiftui
+//
+//  Created by Caio Dias on 2024-12-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
