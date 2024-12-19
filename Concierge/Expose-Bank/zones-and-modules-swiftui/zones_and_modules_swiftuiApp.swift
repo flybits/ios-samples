d66286616365d69b1814d8e34a3ef27534abfefb
Caio Dias <caio.dias@flybits.com>
2024-12-19 00:07:06 -0300

    Add logic to the swiftui sample

--- Concierge/Expose-Bank/zones-and-modules-swiftui/zones_and_modules_swiftuiApp.swift
+++ Concierge/Expose-Bank/zones-and-modules-swiftui/zones_and_modules_swiftuiApp.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import FlybitsConcierge
 
 @main
 struct zones_and_modules_swiftuiApp: App {
+    let projectIdentifier: String = "2CE41988-B1D3-4116-98DD-42FFB8754384"
+    let gatewayURL: String = "https://api.demo.flybits.com"
+    let webServiceURL: String = "https://static-files-concierge.demo.flybits.com/latest"
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
-    
+
     init() {
-        print("Testing")
+        Concierge.enableLogging()
+
+        let config = FlybitsConciergeConfiguration.Builder()
+            .setProjectId(projectIdentifier)
+            .setGatewayUrl(gatewayURL)
+            .setWebService(webServiceURL)
+            .build()
+
+        Concierge.configure(configuration: config, contextPlugins: [])
     }
 }
