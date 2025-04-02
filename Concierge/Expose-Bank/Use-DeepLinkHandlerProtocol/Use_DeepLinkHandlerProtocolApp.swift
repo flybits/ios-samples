b285f6040f798058fc5f8cf89e354484db462674
Caio Dias <caio.dias@flybits.com>
2025-04-02 14:33:19 -0400

    Add new Use-DeepLinkHandlerProtocol app

---
+++ Concierge/Expose-Bank/Use-DeepLinkHandlerProtocol/Use_DeepLinkHandlerProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Use_DeepLinkHandlerProtocolApp.swift
+//  Use-DeepLinkHandlerProtocol
+//
+//  Created by Caio Dias on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct Use_DeepLinkHandlerProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
